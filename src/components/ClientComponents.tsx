29d2c49d5f5788c0c759d8036de1b3695633d09d
fantasiacod <fantasiacode2025@gmail.com>
2026-06-28 23:05:27 +0300

    Add ClientComponents with dynamic imports

---
+++ src/components/ClientComponents.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const PrayerTimes = dynamic(() => import("@/components/PrayerTimes"), { ssr: false });
+const AzkarNotifications = dynamic(() => import("@/components/AzkarNotifications"), { ssr: false });
+
+export default function ClientComponents() {
+  return (
+    <>
+      <PrayerTimes />
+      <AzkarNotifications />
+    </>
+  );
+}
